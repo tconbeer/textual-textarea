26c27e01b2b5d5775f5409756f3e6f7eeb257ade
Ted Conbeer <tconbeer@users.noreply.github.com>
2023-09-20 00:55:41 -0600

    feat: add message on save

--- src/textual_textarea/messages.py
+++ src/textual_textarea/messages.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from typing import Union
+
 from textual.message import Message
 
 
@@ -34,3 +37,13 @@ class TextAreaClipboardError(Message, bubble=True):
     def __init__(self, action: str) -> None:
         super().__init__()
         self.action = action
+
+
+class TextAreaSaved(Message, bubble=True):
+    """
+    Posted when the textarea saved a file successfully.
+    """
+
+    def __init__(self, path: Union[Path, str]) -> None:
+        self.path = str(path)
+        super().__init__()
--- tests/functional_tests/test_open.py
+++ tests/functional_tests/test_open.py
@@ -1,9 +1,11 @@
 from pathlib import Path
+from typing import List
 
 import pytest
 from textual.app import App
+from textual.message import Message
 from textual.widgets import Input
-from textual_textarea import TextArea
+from textual_textarea import TextArea, TextAreaSaved
 
 
 @pytest.mark.parametrize("filename", ["foo.py", "empty.py"])
@@ -38,3 +40,31 @@ async def test_open(data_dir: Path, app: App, filename: str) -> None:
         await pilot.press("ctrl+end")
         assert ta.cursor.pos >= 0
         await pilot.press("enter")
+
+
+@pytest.mark.asyncio
+async def test_save(app: App, tmp_path: Path) -> None:
+    TEXT = "select\n    1 as a,\n    2 as b,\n    'c' as c"
+    p = tmp_path / "text.sql"
+    print(p)
+    messages: List[Message] = []
+    async with app.run_test(message_hook=messages.append) as pilot:
+        ta = app.query_one(TextArea)
+        ta.text = TEXT
+
+        await pilot.press("ctrl+s")
+        save_input = ta.query_one(Input)
+        assert save_input.id and "save" in save_input.id
+        assert save_input.has_focus
+
+        save_input.value = str(p)
+        app.save_screenshot("debug")
+        await pilot.press("enter")
+        await pilot.pause()
+        assert len(messages) > 1
+        assert Input.Submitted in [msg.__class__ for msg in messages]
+        assert TextAreaSaved in [msg.__class__ for msg in messages]
+
+        with open(p, "r") as f:
+            saved_text = f.read()
+        assert saved_text == TEXT
